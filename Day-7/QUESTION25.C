cf7ec7a1825668ae52be7d394c1badbce6e042c3
Aakash rajpoot <akashrajpoot90466@gmail.com>
2026-06-18 15:18:08 +0530

    Add files via upload

---
+++ Day-7/QUESTION25.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+    printf("%d! = %lld\n", n, factorial(n));
+    return 0;
+}
